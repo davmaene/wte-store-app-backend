977e573f89e71bb33e96b860272ef6755a946ad3
davmaene <davidmened@gmail.com>
2024-02-19 11:55:47 +0200

    up --me line

---
+++ controlers/controler.store.js
@@ -0,0 +1,18 @@
+import { Configs } from "../configs/configs.js"
+import { Response } from "../helpers/helper.message.js"
+import { randomLongNumber } from "../helpers/helper.random.js"
+
+export const __controlerStore = {
+    bonentree: async (req, res, next) => {
+        const trans = randomLongNumber({ length: 6 })
+        try {
+
+        } catch (error) {
+            return Response(res, 500, error)
+        }
+    },
+
+    bonsortie: async (req, res, next) => {
+
+    }
+}
