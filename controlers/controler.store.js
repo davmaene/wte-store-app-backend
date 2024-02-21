45786724425b09b674cefbcfaa105cfb66a8a010
davmaene <davidmened@gmail.com>
2024-02-21 16:01:55 +0200

    up me line

--- controlers/controler.store.js
+++ controlers/controler.store.js
@@ -9,8 +9,7 @@ export const __controlerStore = {
     bonentree: async (req, res, next) => {
         const trans = randomLongNumber({ length: 16 })
         const { items } = req.body
-        const {} = req.currentuser;
-        console.log(req.currentuser);
+        const { phone: asphone, uuid, roles, __id, iat, exp, jti } = req.currentuser;
         if (!Array.isArray(items)) return Response(res, 401, "Items must be a type of array !")
 
         try {
@@ -41,8 +40,18 @@ export const __controlerStore = {
             Stores.create({
                 transaction: trans,
                 items: newItesms,
-                createdby: 1
+                createdby: __id
             })
+                .then(str => {
+                    if (str instanceof Stores) {
+                        return Response(res, 200, { transaction: trans, length: newItesms.length, items: newItesms })
+                    } else {
+                        return Response(res, 400, str)
+                    }
+                })
+                .catch(err => {
+                    return Response(res, 503, err)
+                })
         } catch (error) {
             return Response(res, 500, error)
         }
