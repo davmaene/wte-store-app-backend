7c28331a66a561733b278583f0e962701ff57d48
davmaene <davidmened@gmail.com>
2024-02-21 17:46:16 +0200

    up ---

--- controlers/controler.gstore.js
+++ controlers/controler.gstore.js
@@ -1,13 +1,28 @@
 import { Response } from "../helpers/helper.message.js"
+import { GStores } from "../models/model.guichetstores.js"
 
 export const __controlerGstore = {
     bonentree: async (req, res, next) => {
 
     },
     getstore: async (req, res, next) => {
-        const { idguichet } = req.bodym
+        const { idguichet } = req.params
         try {
-            
+            GStores.findOne({
+                where: {
+                    idguichet: parseInt(idguichet)
+                }
+            })
+                .then(st => {
+                    if (st instanceof GStores) {
+                        
+                    } else {
+                        return Response(res, 400, st)
+                    }
+                })
+                .catch(er => {
+                    return Response(res, 503, er)
+                })
         } catch (error) {
             return Response(res, 500, error)
         }
--- routes/routes.store.js
+++ routes/routes.store.js
@@ -1,5 +1,7 @@
 import express from 'express'
 import { __controlerStore } from '../controlers/controler.store.js'
+import { __controlerGstore } from '../controlers/controler.gstore.js'
 
 export const __routesStores = express.Router()
 __routesStores.post("/store/in", __controlerStore.bonentree)
+__routesStores.get("/store/getone/:idguichet", __controlerGstore.getstore)
