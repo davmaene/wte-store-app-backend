eb338d76c4aa7adc91b038273e9ce07d8da4c83f
davmaene <davidmened@gmail.com>
2024-02-21 17:13:19 +0200

    up me line

---
+++ models/model.guichetstores.js
@@ -0,0 +1,52 @@
+import Sequelize from 'sequelize';
+import { Configs } from '../configs/configs.js';
+import { now, nowInUnix } from '../helpers/helper.moment.js';
+import dotenv from 'dotenv';
+
+dotenv.config()
+
+const { ESCAPESTRING } = process.env;
+
+export const GStores = Configs.define('__tbl_gstores', {
+    transaction: {
+        type: Sequelize.STRING,
+        allowNull: false,
+    },
+    items: {
+        type: Sequelize.JSON,
+        allowNull: false,
+        defaultValue: ESCAPESTRING
+    },
+    createdon: {
+        type: Sequelize.STRING,
+        allowNull: false,
+        defaultValue: now({ options: {} })
+    },
+    createdby: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 1
+    },
+    idguichet: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 1
+    },
+    status: {
+        type: Sequelize.INTEGER,
+        allowNull: true,
+        defaultValue: 1
+    }
+}, {
+    timestamps: false,
+    freezeTableName: true
+});
+
+GStores.sync({ alter: true })
+    .then(() => {
+        console.log('=======> Cerated done `GStores` ');
+    })
+    .catch((error) => {
+        console.error('Une erreur s\'est produite lors de la création de la table :', error);
+    });
+
