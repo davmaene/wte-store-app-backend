754b3e630e2fb8696fb7e815055475b30385a440
davmaene <davidmened@gmail.com>
2024-02-21 21:59:24 +0200

    up ---version done

--- controlers/controler.gstore.js
+++ controlers/controler.gstore.js
@@ -1,4 +1,5 @@
 import { Response } from "../helpers/helper.message.js"
+import { randomLongNumber } from "../helpers/helper.random.js";
 import { Guichets } from "../models/model.guichets.js"
 import { GStores } from "../models/model.guichetstores.js"
 import { Produits } from "../models/model.produits.js";
@@ -17,46 +18,48 @@ export const __controlerGstore = {
                 order: [['id', 'DESC']],
             })
             if (store instanceof Stores) {
+                const { items: asitems } = store
                 const newItesms = []
-                for (let index = 0; index < items.length; index++) {
-                    const { idproduit, idunity, qte, prixachat, prixunitaire, fournisseur } = items[index];
-                    const prd = await Produits.findOne({
-                        where: {
-                            id: parseInt(idproduit)
-                        }
-                    })
-                    if (prd instanceof Produits) {
-                        const { prix } = prd;
-                        prd.update({
-                            updatedon: now({ options: {} }),
-                            prix: parseFloat(prixunitaire)
-                        })
-                        newItesms.push({
-                            idproduit,
-                            prix: parseFloat(prixunitaire),
-                            prixachat: parseFloat(prixachat),
-                            fournisseur,
-                            qte,
-                            idunity
-                        })
-                    }
-                }
-                GStores.create({
-                    idguichet: parseInt(idguichet),
-                    transaction: trans,
-                    items: newItesms,
-                    createdby: __id
-                })
-                    .then(str => {
-                        if (str instanceof GStores) {
-                            return Response(res, 200, { transaction: trans, length: newItesms.length, items: newItesms })
-                        } else {
-                            return Response(res, 400, str)
-                        }
-                    })
-                    .catch(err => {
-                        return Response(res, 503, err)
-                    })
+                console.log(asitems);
+                // for (let index = 0; index < items.length; index++) {
+                //     const { idproduit, idunity, qte, prixachat, prixunitaire, fournisseur } = items[index];
+                //     const prd = await Produits.findOne({
+                //         where: {
+                //             id: parseInt(idproduit)
+                //         }
+                //     })
+                //     if (prd instanceof Produits) {
+                //         const { prix } = prd;
+                //         prd.update({
+                //             updatedon: now({ options: {} }),
+                //             prix: parseFloat(prixunitaire)
+                //         })
+                //         newItesms.push({
+                //             idproduit,
+                //             prix: parseFloat(prixunitaire),
+                //             prixachat: parseFloat(prixachat),
+                //             fournisseur,
+                //             qte,
+                //             idunity
+                //         })
+                //     }
+                // }
+                // GStores.create({
+                //     idguichet: parseInt(idguichet),
+                //     transaction: trans,
+                //     items: newItesms,
+                //     createdby: __id
+                // })
+                //     .then(str => {
+                //         if (str instanceof GStores) {
+                //             return Response(res, 200, { transaction: trans, length: newItesms.length, items: newItesms })
+                //         } else {
+                //             return Response(res, 400, str)
+                //         }
+                //     })
+                //     .catch(err => {
+                //         return Response(res, 503, err)
+                //     })
             } else {
                 return Response(res, 400, "The principal store is empty ! so we can not process with the request ")
             }
