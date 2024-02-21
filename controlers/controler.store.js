30321e5a58d78db1ddff30af5b46771fbc5c0530
davmaene <davidmened@gmail.com>
2024-02-21 13:07:34 +0200

    up ---version done

--- controlers/controler.store.js
+++ controlers/controler.store.js
@@ -1,15 +1,24 @@
 import { Configs } from "../configs/configs.js"
 import { Response } from "../helpers/helper.message.js"
 import { randomLongNumber } from "../helpers/helper.random.js"
+import { Produits } from "../models/model.produits.js"
 
 export const __controlerStore = {
     bonentree: async (req, res, next) => {
-        const trans = randomLongNumber({ length: 6 })
+        const trans = randomLongNumber({ length: 16 })
         const { items } = req.body
-        if(!Array.isArray(items)) return Response(res, 401, "Items must be a type of array !")
+        if (!Array.isArray(items)) return Response(res, 401, "Items must be a type of array !")
 
         try {
-            
+            for (let index = 0; index < items.length; index++) {
+                const { idproduit, idunity, qte, prixachat, prixunitaire, fournisseur } = items[index];
+                const prd = await Produits.findOne({
+                    where: {
+                        id: parseInt(idproduit)
+                    }
+                })
+            }
+            return Response(res, 200, { transation: trans, items })
         } catch (error) {
             return Response(res, 500, error)
         }
