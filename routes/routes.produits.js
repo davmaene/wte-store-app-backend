03f4065d9a41e583e7de29093fca7b3e873f328e
davmaene <davidmened@gmail.com>
2024-04-14 23:58:19 +0200

    up me line

--- routes/routes.produits.js
+++ routes/routes.produits.js
@@ -3,6 +3,7 @@ import { __controlerProduits } from '../controlers/controler.produits.js';
 import { __controlerGstore } from '../controlers/controler.gstore.js';
 
 export const __routesProduits = express.Router()
+
 __routesProduits.get("/list", __controlerProduits.list)
 __routesProduits.get("/listinstore/byguichet", __controlerGstore.getlistproduitinstoreparguichet)
 __routesProduits.post("/produit/add", __controlerProduits.add)
