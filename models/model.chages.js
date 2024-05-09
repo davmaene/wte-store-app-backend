2d05d57d4f3849067bba232882af65852b7c1fb1
davmaene <davidmened@gmail.com>
2024-05-09 10:54:05 +0200

    up me line

--- models/model.chages.js
+++ models/model.chages.js
@@ -30,7 +30,7 @@ export const Charges = Configs.define('__tbl_charges', {
 
 Charges.sync({ alter: true })
     .then(() => {
-        console.log('=======> Cerated done `table Config` ');
+        console.log('=======> Cerated done `table Charges` ');
     })
     .catch((error) => {
         console.error('Une erreur s\'est produite lors de la création de la table :', error);
