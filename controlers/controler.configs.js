ed611a6dbc1d0d717f02db10078a5025000d4f09
davmaene <davidmened@gmail.com>
2024-05-20 09:43:47 +0200

    up me line

--- controlers/controler.configs.js
+++ controlers/controler.configs.js
@@ -16,17 +16,20 @@ export const __controlerConfigs = {
     update: async (req, res, next) => {
         const { taux_change, commission_price } = req.body
         if (!taux_change) return Response(res, 401, "this request must have at least commission_price as taux_change as params !")
-        Config.findOrCreate({
+        Config.findOne({
             where: {
                 id: 1
             },
-            defaults: {
-                taux_change: parseFloat(taux_change),
-                commission_price
-            }
         })
-            .then(([result, isnew]) => {
-                return Response(res, 200, result)
+            .then((isnew) => {
+                if (isnew instanceof Config) {
+                    isnew.update({
+                        taux_change: parseFloat(taux_change),
+                        [commission_price]: commission_price ? parseFloat(commission_price) : undefined
+                    })
+                } else {
+                    return Response(res, 400, isnew)
+                }
             })
             .catch((err) => {
                 return Response(res, 500, err)
