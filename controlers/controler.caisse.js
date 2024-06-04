80b0ec02a7951526a054759928e451cbc324abdc
davmaene <davidmened@gmail.com>
2024-06-04 07:56:26 +0200

    up me line

---
+++ controlers/controler.caisse.js
@@ -0,0 +1,13 @@
+import { Response } from "../helpers/helper.message.js";
+
+export const __controlerCaisse = {
+    update: async (req, res, next) => {
+        const { amount, currency } = req.body;
+        if (!amount || !currency) return Response(res, 401, "This request must have at least amount !")
+        try {
+
+        } catch (error) {
+            return Response(res, 500, error)
+        }
+    }
+}
