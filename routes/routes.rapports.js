aa2701174c4506d4127d171e52e3ddde0d1808ff
davmaene <davidmened@gmail.com>
2024-06-18 21:13:24 +0100

    up me line

--- routes/routes.rapports.js
+++ routes/routes.rapports.js
@@ -1,2 +1,8 @@
 import express from "express";
+import { __controlerRapports } from "../controlers/controler.rapport";
 
+export const __routesRapport = express.Router()
+
+__routesRapport.get("/dayly", __controlerRapports.dayly)
+__routesRapport.get("/monthly", __controlerRapports.monthly)
+__routesRapport.get("/yearly", __controlerRapports.yearly)
